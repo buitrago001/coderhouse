d2a0ec0856efb24fc7991b23c99fe8a43bc9a6e2
Andres Buitrago <buitrago001@gmail.com>
2023-04-17 19:26:38 -0500

    Add files via upload

--- preentrega/JavaScript/control.js
+++ preentrega/JavaScript/control.js
@@ -12,6 +12,7 @@ let access;
 let userAccess;
 let inventoryOption;
 let product;
+let totalProducts;
 let productToInclude;
 let end;
 
@@ -19,18 +20,18 @@ let newUser;
 let nombre;
 let apellido;
 let paciente;
-let totalProducts;
 
-//Constant
-const accessMenu = "Bienvenido, Seleccione  \n\ 1) Login, \n\ 2) Registro";
-const userAccessMenu = "Ingrese su Usuario \n\ (usuario: testUsuario o testFuncionario)";
+
+//Constant - menus
+const accessMenu = "Bienvenido, Seleccione  \n\ 1) Login \n\ 2) Registrarse";
+const userAccessMenu = "Ingrese su Usuario \n\ (usuario: testUsuario)";
 
 const userTypeMenu = "Seleccione  \n\ 1) Usuario, \n\ 2) Funcionario";
 const userSelection = "Seleccione Paciente \n\ 1) Paciente1 \n\ 2) Paciente2 \n\ 3) Paciente3 \n\ 4) Paciente4 ";
 
 const inventoryOptionMenuUser = "Seleccione la accion que desea realizar \n\ 1) Consultar Inventario  \n\ 2) Lista Productos Requeridos \n\ 3) Pre-cargar Invetario"
 
-const inventory = "Los productos disponibles son \n\ medicina1 - 5 \n\ medicina2 - 6 \n\ medicina3  \n\ 2 producto1 - 0 \n\ producto5 - 0 \n\ producto8 - 0 "
+const inventory = "Los productos disponibles son \n\ medicina1 - 5 \n\ medicina2 - 6 \n\ medicina3  -2 \n\ producto1 - 0 \n\ producto5 - 0 \n\ producto8 - 0 "
 const neededProducts = "Los productos que se requeiren son \n\ producto1 \n\ producto5 \n\ producto8"
 
 const inventoryOptionMenuEmployee = "Seleccione la accion que desea realizar \n\ 1) Consultar Inventario  \n\ 2) Lista Productos Requeridos \n\ 3) Pre-cargar Invetario"
@@ -43,7 +44,8 @@ do
     access = Number(prompt(accessMenu));
     console.log(access);
     
-    if(access==1){
+    if(access==1)
+    {
         userAccess = prompt(userAccessMenu);
         console.log(userAccess);
 
@@ -52,32 +54,55 @@ do
             inventoryOption = Number(prompt(inventoryOptionMenuUser));
             while(inventoryOption >0 && inventoryOption <4)
             {
-                //switch option to execute
                 switch(inventoryOption){
-                    case 1:
-                        prompt(inventory);
+                    
+                    case 1: //consultar inventario
+                    
+                        inventoryOption = Number( prompt(inventory + " \n\ Que desea hacer a continuacion: \n\ 1) Lista Productos Requeridos \n\ 2) Pre-cargar Invetario \n\ 0) salir"));
+                        console.log(inventoryOption);
+                        if(inventoryOption == 1) inventoryOption =2;
+                        else if(inventoryOption ==2) inventoryOption =3;
+                        else if(inventoryOption != 1 || inventoryOption !=2) inventoryOption=0;
+                        
+                    break; 
+
+                    case 2: // consultar productos con baja cantiadad o sin inventario
+
+                        inventoryOption = Number (prompt(neededProducts + " \n\ Que desea hacer a continuacion: \n\ 1) Consultar Inventario \n\ 2) Pre-cargar Invetario \n\ 0) salir"));
+                        if(inventoryOption == 2) inventoryOption =3;
+                        else if(inventoryOption <1 || inventoryOption >2) inventoryOption=0;
 
                     break;
-                    case 2:
-                        prompt(neededProducts);
-                    break;
+
                     case 3:
-                        productToInclude =Number(prompt("¿cuantos productos va a ingresar?"));
-                        for(let i = 0; i<productToInclude; i++)
-                        {
-                            let product = (prompt("Ingrese el producto " + i));
-                            totalProducts = totalProducts + product;
-                            if(i==totalProducts) alert("Productos ingresados "+ totalProducts);
-                        }
-                    break;
-                    case 4:
-                        access =0;
-                    break;
-                }
-            }
-            
-            access =0;
 
+                        productToInclude =Number(prompt("¿cuantos productos va a ingresar?"));
+
+                        for(let i = 1; i<productToInclude+1; i++)
+                        {
+                            product = (prompt("Ingrese el producto " + i));
+                            console.log(totalProducts);
+                            if(totalProducts == undefined)
+                            {
+                                totalProducts = product;
+                                console.log(totalProducts);
+                            }else 
+                            {
+                                totalProducts = totalProducts + product;
+                            }
+                             
+                        }
+                        alert("Productos ingresados "+ totalProducts);
+                        console.log(inventoryOption);
+                        inventoryOption = Number (prompt("Que desea hacer a continuacion: \n\ 1) Consultar Inventario \n\ 2) Lista Productos Requeridos \n\ 0) salir"));
+                        console.log(inventoryOption);
+                        if(inventoryOption <1 || inventoryOption >3) inventoryOption=0;
+                        console.log(inventoryOption);
+
+                    break;
+                }   
+                access =0;
+            }
         }
         else if(userAccess == "testFuncionario")
         {
@@ -89,7 +114,12 @@ do
             access =0;
 
         }
-     }else if(access ==2)
+        else{
+            alert(" este usuario invalido, reintente")
+            console.log(access);
+        } 
+     }
+     else if(access ==2) // crearcion de usuario nuevo
      {
         nombre = prompt("Ingrese Nombre");
         apellido = prompt ("Ingrese Apellido");
@@ -98,6 +128,8 @@ do
         access = Number(prompt(accessMenu));
      }
 
-}while(access ==1 || access ==2);
+}while(access ==1);
 
 alert("Ha terminado el programa, que tenga un buen dia");
+
+
